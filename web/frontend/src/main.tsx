1c2c8b9c0fd65e2a6bb4a25c70ba9c9e1526886d
Shilong Li <146972613+BICAamy@users.noreply.github.com>
2026-08-18 10:17:12 +0800

    step14: mount gesture console

--- web/frontend/src/main.tsx
+++ web/frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import GestureOverlay from "./GestureOverlay";
 import "./styles.css";
+import "./gesture.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <GestureOverlay />
   </React.StrictMode>,
 );
